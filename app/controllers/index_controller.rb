06f0cd72c20de411b5c5f024aa4e07b6a4386aa5
takumi <takumi@takumi-no-MacBook-Air.local>
2017-03-13 14:22:39 +0900

    for文修正

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,35 @@
+class IndexController < ApplicationController
+  def index
+    keyid     = "a6fe695fd03809023efa81072abccf3b"
+    url       = "https://api.gnavi.co.jp/RestSearchAPI/20150630/"
+    latitude = 35.670083
+    longitude = 139.763267
+    range = 1
+
+    params = {
+        keyid: keyid,
+        format: 'json',
+        latitude: latitude,
+        longitude: longitude,
+        range: range
+    }
+
+    result = open("#{url}?#{URI.encode_www_form(params)}")
+    result = JSON.parse(result.read)
+    #@res = result['rest']['0']
+    if result['total_hit_count'].to_i > 0 then
+      @res = ''
+      @res = result['total_hit_count'] + '件の結果が見つかりました。\n'
+      @res = result['rest']
+      for i in result['rest'].size do
+        @res += result['rest'][i]['id']# + ' ' + result['rest'][i]['name'] + ' ' + result['rest'][i]['access']['line'] + ' ' + result['rest'][i]['access']['station'] + ' ' + result['rest'][i]['access']['walk'] + '分\n'
+      end
+    else
+      @res = '検索結果が見つかりませんでした。'
+    end
+   end
+
+  def show
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#top'
-
+  root 'index#index'
+  get 'index#show' => 'index#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
