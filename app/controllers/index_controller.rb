2b98fdb2788977bb0170ff1d338f7018ca71577c
takumi <takumi@takumi-no-MacBook-Air.local>
2017-03-15 21:44:00 +0900

    ページング未完成

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,13 +1,12 @@
 class IndexController < ApplicationController
   def index
-
-
     params = {
         keyid: @@keyid,
         format: 'json',
         latitude: @@latitude,
         longitude: @@longitude,
-        range: @@range
+        range: @@range,
+        hit_per_page: 12
     }
 
     result = open("#{@@url}?#{URI.encode_www_form(params)}")
@@ -16,22 +15,41 @@ class IndexController < ApplicationController
     #todo 別の一覧ページを作ってそこをindexで表示させる。表示させる内容は/0/1/2のようにurlにページ番号
     items = "..."
     item_total = @res['total_hit_count']
-    #@items = Kaminari.paginate_array(items, total_count: item_total).page(params[:page]).per(10)
+    #@items = Kaminari.paginate_array(items, total_count: item_total).page(params[:page]).per(12)
 
   end
 
-  #todo idをもらってそのidのurlを開く、その時idのapi情報を@resで持ってくる
   def show
-    @id = params[:id]
+    id = params[:id]
 
     params = {
         keyid: @@keyid,
         format: 'json',
-        id: @id
+        id: id
     }
     result = open("#{@@url}?#{URI.encode_www_form(params)}")
     @res = JSON.parse(result.read)
-
-    #render :text => "id = #{params[:id]}"
   end
+
+  def _show
+    params = {
+        keyid: @@keyid,
+        format: 'json',
+        offset_page: @page
+    }
+    result = open("#{@@url}?#{URI.encode_www_form(params)}")
+    @res = JSON.parse(result.read)
+  end
+
+  def get_info
+    @page = params[:page]
+    params = {
+        keyid: @@keyid,
+        format: 'json',
+        offset_page: @page
+    }
+    result = open("#{@@url}?#{URI.encode_www_form(params)}")
+    @res = JSON.parse(result.read)
+  end
+
 end
